2dd57331997b4697b790596384d0eb62fe045d11
Kasun Thennakoon <tmkasun@gmail.com>
2023-10-09 22:52:25 -0400

    add ganalytics

--- src/layouts/root.tsx
+++ src/layouts/root.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Figtree } from "next/font/google";
+import Script from "next/script";
 
 const figtree = Figtree({ subsets: ["latin"] });
 
@@ -14,7 +15,22 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className={`grow flex flex-col ${figtree.className}`}>
-            {children}
-        </div>);
+        <>
+            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-K48WB539H5" />
+            <Script id="google-analytics">
+                {
+                    `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                  
+                    gtag('config', 'G-K48WB539H5');
+        `
+                }
+            </Script>
+            <div className={`grow flex flex-col ${figtree.className}`}>
+                {children}
+            </div>
+        </>
+    );
 }
